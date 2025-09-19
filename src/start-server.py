19c85be295469f5d97341639bf411c4efb1b679a
FranInfanti <franciso.infanti@gmail.com>
2025-09-19 03:31:20 +0000

    feat: Add SIGINT handler for graceful exit

--- src/start-server.py
+++ src/start-server.py
@@ -1,8 +1,25 @@
 import sys
 
+from socket import socket, AF_INET, SOCK_DGRAM
+from signal import SIGINT, signal
+from types import FrameType
+
 from lib.server import Server
 from lib.flags import SERVER_FLAGS
 from lib.utils import split
+from lib.protocolo_amcgf import *
+
+def sigint_handler(_: int, frame: FrameType | None):
+    server_socket = frame.f_locals['server_socket']
+    
+    try:
+        server_socket.close()
+    except:
+        print(f'\nError: Socket {server_socket} could not be closed')
+        sys.exit(-1)
+    
+    print('\nGraceful Exit')
+    sys.exit(0)    
 
 def process_args(args: list[str]):
     server = Server()
@@ -23,9 +40,49 @@ def process_args(args: list[str]):
 
     return server
 
+def run(server: Server):
+    server_socket = socket(AF_INET, SOCK_DGRAM)
+    server_socket.bind((server.host, server.port))
+
+    while True:
+        # Esperar y manejar paquetes ...
+        pass
+
+    server_socket.close()
+
 if __name__ == '__main__':
+
+    # Piso la señal de SIGINT
+    signal(SIGINT, sigint_handler)
+
     args = split(sys.argv)
 
     server = process_args(args)
 
-    print(server)
+    run(server=server)
+
+"""
+Esto me devolvio GPT quizas sirva de algo
+"""
+def handle_packet(pkt: Datagrama, state) -> list[Datagrama]:
+    out: list[Datagrama] = []
+    if pkt.typ == MsgType.HELLO:
+        params = payload_decode(pkt.payload)
+        # valida op/name/size/proto...
+        out.append(make_ok({"transfer_id": state.alloc_tid()}, ver=pkt.ver))
+        state.begin(params, ver=pkt.ver)
+    elif pkt.typ == MsgType.DATA:
+        if pkt.ver == VER_SW:
+            if pkt.seq == state.expected_seq and state.check_and_write(pkt.payload):
+                state.expected_seq ^= 1
+            # en S&W se ACKea el último válido (puntual)
+            out.append(make_ack(state.expected_seq ^ 1, ver=pkt.ver))
+        else:  # VER_GBN
+            if pkt.seq == state.expected_seq and state.check_and_write(pkt.payload):
+                state.expected_seq += 1
+            # en GBN ACK acumulativo del último in-order
+            out.append(make_ack(state.expected_seq - 1, ver=pkt.ver))
+    elif pkt.typ == MsgType.BYE:
+        state.finish()
+        out.append(make_ok(ver=pkt.ver))
+    return out
