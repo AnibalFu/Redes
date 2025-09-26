c371e2895df8c67faf6785d93af3935f699498f9
FranInfanti <francisco.infanti@gmail.com>
2025-09-26 03:11:18 +0000

    refactor: Refactor upload, download and server code

--- src/lib/datagram_sending.py
+++ src/lib/datagram_sending.py
@@ -1,5 +1,8 @@
-from lib.protocolo_amcgf import VER_SW, Datagrama, MsgType, make_bye, make_data, make_hello
+from asyncio.trsock import _RetAddress
+from socket import socket
+from pyparsing import Callable
 
+from lib.protocolo_amcgf import VER_SW, Datagrama, MsgType, make_bye, make_data, make_hello
 
 def send_content(sender_socket, receiver_addr, content, chunk_size, timeout=2):
     sender_socket.settimeout(timeout)
@@ -26,29 +29,62 @@ def send_content(sender_socket, receiver_addr, content, chunk_size, timeout=2):
         seq += 1
     sender_socket.settimeout(None)  # Restablece el timeout
 
+def send_hello(sender_socket: socket, addr: _RetAddress, bufsize: int, proto: str):
+    """Envía un datagrama HELLO y espera un HELLO de respuesta."""
+    
+    try:
+        encoded = make_hello(proto=proto).encode()
+    except Exception:
+        raise
 
-def send_hello(sender_socket, receiver_addr, buf):
-    hello = make_hello(proto="SW")
-    sender_socket.sendto(hello.encode(), receiver_addr)
-    ans, _ = sender_socket.recvfrom(buf)
-    resp = Datagrama.decode(ans)
-    assert resp.typ == MsgType.HELLO, "Esperaba HELLO ACK"
-    print("Recibido HELLO ACK")
+    sender_socket.sendto(encoded, addr)
 
+    data, _ = sender_socket.recvfrom(bufsize)
+    
+    try:
+        datagram = Datagrama.decode(data)
+    except Exception:
+        raise
 
-def send_request(request_maker, sender_socket, receiver_addr, filename):
-    request = request_maker(filename, 0, VER_SW)
-    sender_socket.sendto(request.encode(), receiver_addr)
-    ans, _ = sender_socket.recvfrom(4096)
-    resp = Datagrama.decode(ans)
-    assert resp.typ == MsgType.OK, "Esperaba OK tras REQUEST"
+    assert datagram.typ == MsgType.HELLO, "Expecting HELLO ACK"
 
+def send_request(make_request: Callable, sender_socket: socket, addr: _RetAddress, filename: str):
+    """Envía un datagrama REQUEST y espera un OK de respuesta. Devuelve la nueva dirección (ip, puerto) del servidor."""
+        
+    try:
+        encoded = make_request(filename, 0, VER_SW).encode()
+    except Exception:
+        raise
 
-def send_bye(sender_socket, receiver_addr, buf):
-    bye = make_bye(VER_SW)
-    sender_socket.sendto(bye.encode(), receiver_addr)
-    ans, _ = sender_socket.recvfrom(buf)
-    resp = Datagrama.decode(ans)
-    assert resp.typ == MsgType.OK, "Esperaba OK tras BYE"
-    print("Transferencia finalizada correctamente")
+    sender_socket.sendto(encoded, addr)
 
+    data, new_addr = sender_socket.recvfrom(4096)
+
+    try:
+        datagram = Datagrama.decode(data)
+    except Exception:
+        raise
+
+    assert datagram.typ == MsgType.OK, "Expecting OK after REQUEST"
+    
+    return new_addr
+
+def send_bye(sender_socket: socket, receiver_addr: _RetAddress, bufsize: int):
+    """Envía un datagrama BYE y espera un OK de respuesta."""
+        
+    try:
+        encoded = make_bye(VER_SW).encode()
+    except Exception:
+        raise
+    
+    sender_socket.sendto(encoded, receiver_addr)
+
+    data, _ = sender_socket.recvfrom(bufsize)
+    
+    try:
+        datagram = Datagrama.decode(data)
+    except Exception:
+        raise
+
+    assert datagram.typ == MsgType.OK, "Expecting OK after BYE"
+    sender_socket.close()
--- src/start-server.py
+++ src/start-server.py
@@ -1,3 +1,4 @@
+from asyncio.trsock import _RetAddress
 import sys
 import threading
 import random
@@ -9,103 +10,158 @@ from argparse import ArgumentParser, Namespace, RawDescriptionHelpFormatter
 
 from lib.datagram_sending import send_content
 from lib.server import Server
-from lib.flags import SERVER_FLAGS
 from lib.protocolo_amcgf import *
 
 def sigint_handler(_: int, frame: FrameType | None):
+    """Handle SIGINT for graceful shutdown."""
+
     server_socket = frame.f_locals['server_socket']
     try:
         server_socket.close()
     except:
         print(f'\nError: Socket {server_socket} could not be closed')
         sys.exit(-1)
+    
     print('\nGraceful Exit')
     sys.exit(0)    
 
 def define_flags():
+    """Define command line flags."""
+
     parser = ArgumentParser(description='File server program', formatter_class=RawDescriptionHelpFormatter)
+    
     parser.add_argument('-v', '--verbose', required=False, action='store_true', help='increase output verbosity')
     parser.add_argument('-q', '--quiet', required=False, action='store_true', help='decrease output verbosity')
     parser.add_argument('-H', '--host', required=False, type=str, metavar='HOST', help='server IP address')
     parser.add_argument('-p', '--port', required=False, type=int, metavar='PORT', help='server port')
     parser.add_argument('-s', '--storage', required=False, type=str, metavar='DIRPATH', help='destination file path')
+    
     return parser
 
 def process_args(args: Namespace):
+    """Process command line arguments and return a configured Server instance."""
+
     server = Server()
+    
     server.verbose = args.verbose
     server.quiet = args.quiet
     server.host = args.host if args.host else server.host
     server.port = args.port if args.port else server.port    
     server.storage = args.storage if args.storage else server.storage
+    
     return server
 
-def handle_upload(sock, client_addr):
+def handle_upload(handle_socket: socket, client_addr: _RetAddress):
     ok = make_ok(ver=VER_SW)
-    sock.sendto(ok.encode(), client_addr)
-    print(f"Upload handler en puerto {sock.getsockname()[1]} para {client_addr}")
+    
+    # Hay que ver que hacer en este caso de error.
+    try:
+        encoded = ok.encode()
+    except Exception as e:
+        print(f"Error: Error while trying to encode Datagram: {e}")
+        return
+    
+    handle_socket.sendto(encoded, client_addr)
+    print(f"Upload handler en puerto {handle_socket.getsockname()[1]} para {client_addr}")
+    
     filedata = b""
     while True:
-        data, addr = sock.recvfrom(4096)
-        datagrama = Datagrama.decode(data)
-        if datagrama.typ == MsgType.DATA:
-            filedata += datagrama.payload
-            ack = make_ack(acknum=datagrama.seq + 1, ver=VER_SW)
-            sock.sendto(ack.encode(), addr)
-            if not (datagrama.flags & FLAG_MF):
+        data, client_addr = handle_socket.recvfrom(4096)
+
+        # Hay que ver que hacer en este caso de error.
+        try:
+            datagram = Datagrama.decode(data)
+        except Exception as e:
+            print(f"Error: Error while trying to decode Datagram: {e}")
+            return
+
+        if datagram.typ == MsgType.DATA:
+            filedata += datagram.payload
+            ack = make_ack(acknum=datagram.seq + 1, ver=VER_SW)
+
+            # Hay que ver que hacer en este caso de error.
+            try:
+                encoded = ack.encode()
+            except Exception as e:
+                print(f"Error: Error while trying to encode ACK Datagram: {e}")
+                return
+
+            handle_socket.sendto(encoded, client_addr)
+
+            if not (datagram.flags & FLAG_MF):
                 print("Archivo recibido completo, esperando BYE...")
-        elif datagrama.typ == MsgType.BYE:
+        
+        elif datagram.typ == MsgType.BYE:
             print("BYE recibido, guardando archivo y respondiendo OK")
             ok = make_ok(ver=VER_SW)
-            sock.sendto(ok.encode(), addr)
-            break
-    print(filedata)
-    sock.close()
 
-def handle_download(sock, client_addr, filename):
+            # Hay que ver que hacer en este caso de error.
+            try:
+                encoded = ok.encode()
+            except Exception as e:
+                print(f"Error: Error while trying to encode OK Datagram: {e}")
+                return
+
+            handle_socket.sendto(encoded, client_addr)
+            break
+    
+    print(filedata)
+    handle_socket.close()
+
+def handle_download(handle_socket: socket, client_addr: _RetAddress, filename: str):
     ok = make_ok(ver=VER_SW)
-    sock.sendto(ok.encode(), client_addr)  # Enviar OK desde el nuevo socket
-    print(f"Download handler en puerto {sock.getsockname()[1]} para {client_addr}")
-    with open(filename, "rb") as f:
-        content = f.read()
-    send_content(sock, client_addr, content, chunk_size=6)
-    sock.close()
+
+    try:
+        encoded = ok.encode()
+    except Exception as e:
+        print(f"Error: Error while trying to encode Datagram: {e}")
+        return
+
+    handle_socket.sendto(encoded, client_addr)  # Enviar OK desde el nuevo socket
+    
+    print(f"Download handler en puerto {handle_socket.getsockname()[1]} para {client_addr}")
+    with open(filename, "rb") as file:
+        content = file.read()
+    
+    send_content(handle_socket, client_addr, content, chunk_size=6)
+    
+    handle_socket.close()
 
 def run(server: Server):
     server_socket = socket(AF_INET, SOCK_DGRAM)
     server_socket.bind((server.host, server.port))
-    print(f"Servidor escuchando en {server.host}:{server.port}")
+    
+    print(f"Server listening at {server.host}:{server.port}")
 
     while True:
-        data, sender_address = server_socket.recvfrom(4096)
+        data, client_addr = server_socket.recvfrom(4096)
         if len(data) < HDR_SIZE:
-            print("Mensaje de control recibido:", data)
+            print("Control message received:", data)
             continue
-
+           
         try:
-            datagrama = Datagrama.decode(data)
+            datagram = Datagrama.decode(data)
         except Exception as e:
-            print(f"Error al decodificar datagrama: {e}")
-            continue
+            raise
 
-        if datagrama.typ == MsgType.REQUEST_UPLOAD:
-            print("UPLOAD recibido")
-            sock = socket(AF_INET, SOCK_DGRAM)
-            sock.bind(('', 0))  # Puerto libre asignado por el SO
-            threading.Thread(target=handle_upload, args=(sock, sender_address), daemon=True).start()
+        if datagram.typ == MsgType.REQUEST_UPLOAD:
+            handle_socket = socket(AF_INET, SOCK_DGRAM)
+            handle_socket.bind(('', 0))
+            
+            threading.Thread(target=handle_upload, args=(handle_socket, client_addr), daemon=True).start()
 
-        elif datagrama.typ == MsgType.REQUEST_DOWNLOAD:
-            print("DOWNLOAD recibido")
-            filename = datagrama.payload.decode().split('=', maxsplit=1)[1]
-            sock = socket(AF_INET, SOCK_DGRAM)
-            sock.bind(('', 0))  # Puerto libre asignado por el SO
-            threading.Thread(target=handle_download, args=(sock, sender_address, filename), daemon=True).start()
+        elif datagram.typ == MsgType.REQUEST_DOWNLOAD:
+            filename = datagram.payload.decode().split('=', maxsplit=1)[1]
+            
+            handle_socket = socket(AF_INET, SOCK_DGRAM)
+            handle_socket.bind(('', 0))
 
-    server_socket.close()
+            threading.Thread(target=handle_download, args=(handle_socket, client_addr, filename), daemon=True).start()
 
 if __name__ == '__main__':
     signal(SIGINT, sigint_handler)
+
     parser = define_flags()
     args = parser.parse_args()
-    server = process_args(args)
-    run(server=server)
+    
+    run(server=process_args(args))
