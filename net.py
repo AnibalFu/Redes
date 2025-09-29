ae9270978bf4c94cd47740b5f2eeda4f83e01f44
FranInfanti <francisco.infanti@gmail.com>
2025-09-29 21:19:48 +0000

    feat: Add mininet topology

--- net.py
+++ net.py
@@ -1,21 +1,28 @@
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.link import TCLink
+from mininet.node import Controller
 from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.topo import Topo
 
-class FTTopo(Topo):
-    def build(self):
-        s = self.addSwitch('s1')
-        srv = self.addHost('h1')  # servidor
-        c1  = self.addHost('h2')  # cliente 1
-        c2  = self.addHost('h3')  # cliente 2
-        link = dict(bw=10, delay='10ms', loss=10)  # 10% pérdida
-        self.addLink(srv, s, **link)
-        self.addLink(c1,  s, **link)
-        self.addLink(c2,  s, **link)
+net = Mininet(controller=Controller, link=TCLink)
 
-if __name__ == '__main__':
-    net = Mininet(topo=FTTopo(), link=TCLink)
-    net.start()
-    CLI(net)
-    net.stop()
+c0 = net.addController('c0')
+
+# Add one server host
+h1 = net.addHost('h1', ip='10.0.0.1/24')
+
+# Add two client host
+h2 = net.addHost('h2', ip='10.0.0.2/24')
+h3 = net.addHost('h3', ip='10.0.0.3/24')
+
+s1 = net.addSwitch('s1')
+
+net.addLink(h1, s1, loss=0)
+net.addLink(h2, s1, loss=0)
+net.addLink(h3, s1, loss=0)
+
+net.start()
+
+CLI(net)
+
+net.stop()
