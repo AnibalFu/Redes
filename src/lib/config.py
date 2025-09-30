36e1b9c0fbf04128cb72947ed61f719773168d50
camila <cfigueroac@fi.uba.ar>
2025-09-30 01:07:35 -0300

    change max file size constanbt

--- src/lib/config.py
+++ src/lib/config.py
@@ -1,4 +1,4 @@
 
 RTO = 0.025 # Retraso de transmision 25 ms
 TIMEOUT_MAX = 0.05 # Timeout maximo 50 ms
-MAX_FILE_SIZE = 10 * 1024 * 1024  # Tamaño máximo de archivo 10 MB
+MAX_FILE_SIZE = 256 * 1024 * 1024  # Tamaño máximo de archivo 256 MB
